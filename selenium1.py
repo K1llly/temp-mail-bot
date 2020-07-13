e18698dc5ba8d798260dcc357c75604f080a59c4
K1llly <gokaygoktas060708@gmail.com>
2020-07-13 17:40:42 +0300

    temp-mail-bot

--- selenium1.py
+++ selenium1.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 import pyperclip
+import os
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -8,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 f = open("tempmail", "a")
 
-path = r"C:\Users\Killly\Desktop\python selenium\chromedriver.exe"
+path = "chromedriver.exe"
 driver = webdriver.Chrome(path)
 
 driver.get("https://www.temp-mail.org")
@@ -27,6 +28,7 @@ try:
     f.write(paste)
     f.write("\n")
     f.close()
+    os.system("python pygame1.py")
 except:
     driver.quit()
 
